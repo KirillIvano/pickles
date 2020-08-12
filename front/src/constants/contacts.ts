264bde3809666c1bcd3f45485abad37251555f60
KirillIvano <ivannadez@yandex.ru>
2020-08-13 01:56:42 +0300

    added common constants

---
+++ front/src/constants/contacts.ts
@@ -0,0 +1,2 @@
+export const MAIN_PHONE_NUMBER = '8 999 999 99 99';
+export const MAIN_EMAIL = 'info@aglobell.ru';
---
+++ front/src/constants/nav.ts
@@ -0,0 +1,15 @@
+export const NAV_ITEMS = [
+    {
+        path: '/catalog',
+        name: 'Каталог',
+    },
+    {
+        path: '/delivery',
+        name: 'Доставка',
+    },
+    {
+        path: '/cart',
+        name: 'Корзина',
+    },
+];
+
